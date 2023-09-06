ae408da03f762ba1f91d37160df85ff2cfc918ca
nordluma <maxnordlund.95@hotmail.com>
2023-09-06 20:14:20 +0300

    feat(cl_server): set up a `TcpListener`

--- cl_server/src/main.rs
+++ cl_server/src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::{Context, Result};
+use tokio::net::TcpListener;
+
+#[tokio::main(flavor = "multi_thread", worker_threads = 2)]
+async fn main() -> Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:42069").await?;
+
+    let (socket, _) = listener
+        .accept()
+        .await
+        .context("Could not get the client")?;
+
+    Ok(())
 }
