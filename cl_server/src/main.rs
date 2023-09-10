e06baaad022f62f1f6f105fc4440a6c2babb4e80
nordluma <maxnordlund.95@hotmail.com>
2023-09-10 19:08:13 +0300

    feat: implement a task manager for keeping state
    Add a task manager to keep state while adding new tasks and executing
    tasks.

--- cl_server/src/main.rs
+++ cl_server/src/main.rs
@@ -1,4 +1,4 @@
-use std::{io::Cursor, path::PathBuf};
+use std::{io::Cursor, path::PathBuf, sync::Arc, time::Duration};
 
 use anyhow::{Context, Result};
 use ciborium::from_reader;
@@ -6,6 +6,8 @@ use serde::Deserialize;
 use tokio::{
     io::AsyncReadExt,
     net::{TcpListener, TcpStream},
+    sync::Mutex,
+    time::sleep,
 };
 
 #[derive(Debug, Deserialize)]
@@ -23,9 +25,44 @@ struct Task {
     cwd: Option<PathBuf>,
 }
 
+struct TaskManager {
+    tasks: Arc<Mutex<Vec<Task>>>,
+}
+
+impl TaskManager {
+    fn new() -> TaskManager {
+        TaskManager {
+            tasks: Arc::new(Mutex::new(Vec::new())),
+        }
+    }
+
+    async fn add_task(&mut self, task: Task) {
+        let mut tasks = self.tasks.lock().await;
+        println!("Addign task: {:?}", task);
+        tasks.push(task);
+    }
+
+    async fn execute_tasks(&mut self) {
+        let wait = Duration::from_secs(2);
+        let mut tasks = self.tasks.lock().await;
+        println!("Running all tasks");
+
+        while let Some(task) = tasks.pop() {
+            println!("Executing task: {:?}", task);
+            sleep(wait).await;
+        }
+    }
+
+    async fn show_all_tasks(&self) {
+        let tasks = self.tasks.lock().await;
+        println!("{:?}", tasks);
+    }
+}
+
 #[tokio::main(flavor = "multi_thread", worker_threads = 2)]
 async fn main() -> Result<()> {
     let listener = TcpListener::bind("127.0.0.1:42069").await?;
+    let mut task_manager = TaskManager::new();
 
     let (socket, _) = listener
         .accept()
@@ -36,18 +73,17 @@ async fn main() -> Result<()> {
 
     match task {
         Command::Add(task) => {
-            println!("Addign task: {:?}", task);
+            task_manager.add_task(task).await;
         }
         Command::Run => {
-            println!("Running all tasks");
+            task_manager.execute_tasks().await;
         }
         Command::Kill => {
             println!("Killing task");
         }
-        Command::Show => {
-            println!("showing tasks");
-        }
+        Command::Show => task_manager.show_all_tasks(),
     }
+
     Ok(())
 }
 
