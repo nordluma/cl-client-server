f43d4761a29e91f9d724de53c4952e267dbaf650
nordluma <maxnordlund.95@hotmail.com>
2023-10-29 21:48:32 +0200

    feat(cl_lib): initialize client tcp stream

--- cl_lib/src/network/tcp.rs
+++ cl_lib/src/network/tcp.rs
@@ -9,3 +9,12 @@ pub async fn init_listener(settings: (&str, &str)) -> Result<Listener, Box<dyn s
 
     Ok(Box::new(listener))
 }
+
+pub async fn init_client_stream(
+    host: &str,
+    port: &str,
+) -> Result<GenericStream, Box<dyn std::error::Error>> {
+    let sender = TcpStream::connect(format!("{}:{}", host, port)).await?;
+
+    Ok(Box::new(sender))
+}
