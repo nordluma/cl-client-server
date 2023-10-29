cfef2d577c6abdff972a221532b8174d31e9b4e1
nordluma <maxnordlund.95@hotmail.com>
2023-10-29 21:56:25 +0200

    refactor(cl_client): use function declared in `cl_lib`
    Initialize tcp stream, send message and receive response with the
    function declared in `cl_lib`.

--- cl_client/src/main.rs
+++ cl_client/src/main.rs
@@ -1,39 +1,31 @@
-use anyhow::{Context, Result};
-use ciborium::into_writer;
 use clap::Parser;
-use tokio::{io::AsyncWriteExt, net::TcpStream};
 
 use cl_client::cli::args::{Cli, Command};
-use cl_lib::message::Message;
+use cl_lib::network::{init_client_stream, receive_response, send_message};
 
 #[tokio::main(flavor = "current_thread")]
 async fn main() {
-    // Parse arguments
+    // 1. Read configurations
+
+    // 2. Parse arguments
     let opts = Cli::parse();
 
+    // 3. Start TCP server
+
+    // 4. Send task
     if let Err(e) = send_task(opts.command).await {
         eprintln!("{}", e);
     };
 }
 
-async fn send_task(cmd: Command) -> Result<()> {
-    let mut sender = TcpStream::connect("127.0.0.1:42069")
-        .await
-        .context("could not connect to server")?;
+async fn send_task(cmd: Command) -> anyhow::Result<()> {
+    let mut sender = init_client_stream("127.0.0.1", "42069").await.unwrap();
 
-    let msg = to_cbor(&cmd.into()).await?;
+    send_message(cmd, &mut sender).await.unwrap();
 
-    sender
-        .write_all(&msg)
-        .await
-        .context("could not send the message")?;
+    let res = receive_response(&mut sender).await.unwrap();
+
+    println!("{:?}", res);
 
     Ok(())
-}
-
-async fn to_cbor(cmd: &Message) -> anyhow::Result<Vec<u8>> {
-    let mut buf = vec![];
-    into_writer(&cmd, &mut buf).context("could not serialize task")?;
-
-    Ok(buf)
 }
