6bd1ee4dbf1a41894edd593dd97d1b6815d24ff0
nordluma <maxnordlund.95@hotmail.com>
2023-10-29 20:42:50 +0200

    fix(cl_lib): return a `Listener` instead of a `GenericStream`

--- cl_lib/src/network/tcp.rs
+++ cl_lib/src/network/tcp.rs
@@ -1,10 +1,9 @@
 use tokio::net::{TcpListener, TcpStream};
 
-type GenericStream = Box<TcpStream>;
+pub type GenericStream = Box<TcpStream>;
+pub type Listener = Box<TcpListener>;
 
-pub async fn init_listener(
-    settings: (&str, &str),
-) -> Result<GenericStream, Box<dyn std::error::Error>> {
+pub async fn init_listener(settings: (&str, &str)) -> Result<Listener, Box<dyn std::error::Error>> {
     let (host, port) = settings;
     let listener = TcpListener::bind(format!("{}:{}", host, port)).await?;
 
