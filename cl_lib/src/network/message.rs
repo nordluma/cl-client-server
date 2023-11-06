57e70ac0df245b844ca3338ec5083501e3e47d0e
nordluma <maxnordlund.95@hotmail.com>
2023-11-06 17:36:57 +0200

    refactor(cl_lib): use errors defined in `cl_lib`

--- cl_lib/src/network/message.rs
+++ cl_lib/src/network/message.rs
@@ -1,51 +1,53 @@
 use std::io::Cursor;
 
+use anyhow::Context;
 use ciborium::{from_reader, into_writer};
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
 
-use crate::message::{Message, Response};
+use crate::{
+    error::ClError,
+    message::{Message, Response},
+};
 
 use super::GenericStream;
 
 const PACKET_SIZE: usize = 1500;
 
-pub async fn send_message<T>(
-    message: T,
-    stream: &mut GenericStream,
-) -> Result<(), Box<dyn std::error::Error>>
+pub async fn send_message<T>(message: T, stream: &mut GenericStream) -> Result<(), ClError>
 where
     T: Into<Message>,
 {
     let mut buf = vec![];
     let message: Message = message.into();
-    into_writer::<Message, _>(&message, &mut buf)?;
+    into_writer::<Message, _>(&message, &mut buf)
+        .context("Failed to serialize message")
+        .map_err(ClError::SerializationError)?;
 
     write_bytes(&buf, stream).await
 }
 
-pub async fn send_response<T>(
-    response: T,
-    stream: &mut GenericStream,
-) -> Result<(), Box<dyn std::error::Error>>
+pub async fn send_response<T>(response: T, stream: &mut GenericStream) -> Result<(), ClError>
 where
     T: Into<Response>,
 {
     let mut buf = vec![];
     let response: Response = response.into();
-    into_writer::<Response, _>(&response, &mut buf)?;
+    into_writer::<Response, _>(&response, &mut buf)
+        .context("Failed to serialize response")
+        .map_err(ClError::SerializationError)?;
 
     write_bytes(&buf, stream).await
 }
 
-pub async fn receive_message(
-    stream: &mut GenericStream,
-) -> Result<Message, Box<dyn std::error::Error>> {
+pub async fn receive_message(stream: &mut GenericStream) -> Result<Message, ClError> {
     let bytes = read_bytes(stream).await?;
     if bytes.is_empty() {
-        return Err("Received empty message".into());
+        return Err(ClError::ConnectionError("Received zero bytes".into()));
     }
 
-    let message = from_reader::<Message, Cursor<Vec<u8>>>(Cursor::new(bytes))?;
+    let message = from_reader::<Message, Cursor<Vec<u8>>>(Cursor::new(bytes))
+        .context("Failed to deserialize message")
+        .map_err(ClError::DeserializationError)?;
 
     Ok(message)
 }
@@ -63,26 +65,26 @@ pub async fn receive_response(
     Ok(response)
 }
 
-pub async fn write_bytes(
-    bytes: &[u8],
-    stream: &mut GenericStream,
-) -> Result<(), Box<dyn std::error::Error>> {
+pub async fn write_bytes(bytes: &[u8], stream: &mut GenericStream) -> Result<(), ClError> {
     let payload_length = bytes.len() as u32;
 
     // TODO: add error handling
-    stream.write_u32(payload_length).await?;
+    stream
+        .write_u32(payload_length)
+        .await
+        .map_err(ClError::IOError)?;
 
     for chunk in bytes.chunks(PACKET_SIZE) {
         // TODO: add error handling
-        stream.write_all(chunk).await?;
+        stream.write_all(chunk).await.map_err(ClError::IOError)?;
     }
 
     Ok(())
 }
 
-pub async fn read_bytes(stream: &mut GenericStream) -> Result<Vec<u8>, Box<dyn std::error::Error>> {
+pub async fn read_bytes(stream: &mut GenericStream) -> Result<Vec<u8>, ClError> {
     // TODO: add error handling
-    let payload_length = stream.read_u32().await? as usize;
+    let payload_length = stream.read_u32().await.map_err(ClError::IOError)? as usize;
     let mut payload_bytes = Vec::with_capacity(payload_length);
 
     while payload_bytes.len() < payload_length {
@@ -94,10 +96,15 @@ pub async fn read_bytes(stream: &mut GenericStream) -> Result<Vec<u8>, Box<dyn s
         };
 
         // TODO: add error handling
-        let received_bytes = stream.read_exact(&mut chunk).await?;
+        let received_bytes = stream
+            .read_exact(&mut chunk)
+            .await
+            .map_err(ClError::IOError)?;
 
         if received_bytes == 0 {
-            return Err("Lost connection to client".into());
+            return Err(ClError::ConnectionError(
+                "Connection went away while receiving bytes".into(),
+            ));
         }
 
         payload_bytes.append(&mut chunk);
--- cl_lib/src/network/tcp.rs
+++ cl_lib/src/network/tcp.rs
@@ -1,20 +1,28 @@
 use tokio::net::{TcpListener, TcpStream};
 
+use crate::error::ClError;
+
 pub type GenericStream = Box<TcpStream>;
 pub type Listener = Box<TcpListener>;
 
-pub async fn init_listener(settings: (&str, &str)) -> Result<Listener, Box<dyn std::error::Error>> {
+pub async fn init_listener(settings: (&str, &str)) -> Result<Listener, ClError> {
     let (host, port) = settings;
-    let listener = TcpListener::bind(format!("{}:{}", host, port)).await?;
+    let listener = TcpListener::bind(format!("{}:{}", host, port))
+        .await
+        .map_err(|err| ClError::ConnectionError(format!("Could not start listener: {}", err)))?;
 
     Ok(Box::new(listener))
 }
 
-pub async fn init_client_stream(
-    host: &str,
-    port: &str,
-) -> Result<GenericStream, Box<dyn std::error::Error>> {
-    let sender = TcpStream::connect(format!("{}:{}", host, port)).await?;
+pub async fn init_client_stream(host: &str, port: &str) -> Result<GenericStream, ClError> {
+    let sender = TcpStream::connect(format!("{}:{}", host, port))
+        .await
+        .map_err(|err| {
+            ClError::ConnectionError(format!(
+                "Could not establish connection with server: {}",
+                err
+            ))
+        })?;
 
     Ok(Box::new(sender))
 }
